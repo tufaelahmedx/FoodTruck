cf7ef96077cecb9812c807715d2f36c23bd26666
tufaelahmedx <tufaelahmedx@gmail.com>
2024-09-24 02:52:16 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Elements from './Components/Elements'
+import Cards from './Components/cards'
+
+
+function App() {
+
+  
+
+  return (
+
+    <div>
+      <div >
+      <Elements />
+    </div>
+
+    <div>
+      <Cards />
+      </div>
+
+    </div>
+    
+  )
+}
+
+export default App
